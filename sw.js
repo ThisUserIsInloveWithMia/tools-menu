c77752b93af1333363ad3b408f6b26539c10356c
ThisUserIsInloveWithMia <143936950+ThisUserIsInloveWithMia@users.noreply.github.com>
2025-09-25 07:38:26 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,29 @@
+const CACHE_NAME = "app-cache-v2"; // bump version when updating
+
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open("app-cache").then(cache => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
         "index.html",
         "DSIRProcessor_v6_3_UI_layout.html",
         "InvoiceGenerator.html",
+        "AutoPMIX.html",
         "icon-192.png",
         "icon-512.png",
         "manifest.json"
       ]);
     })
   );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clientsClaim();
 });
 
 self.addEventListener("fetch", e => {
