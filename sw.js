5a5be533bee7faecf723c92358cf1e440f415ba9
ThisUserIsInloveWithMia <143936950+ThisUserIsInloveWithMia@users.noreply.github.com>
2025-09-26 03:24:59 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,35 @@
-const CACHE_NAME = "app-cache-v2"; // bump version when updating
-
 self.addEventListener("install", e => {
-  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open("app-cache").then(cache => {
       return cache.addAll([
         "index.html",
         "DSIRProcessor_v6_3_UI_layout.html",
         "InvoiceGenerator.html",
-        "AutoPMIX.html",
+        "AutoPMIX.html",        // added third tool
         "icon-192.png",
         "icon-512.png",
         "manifest.json"
       ]);
     })
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(
+        keys.filter(key => key !== "app-cache").map(key => caches.delete(key))
+      )
     )
   );
-  self.clientsClaim();
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request, { ignoreSearch: true }).then(response => {
+      return response || fetch(e.request);
+    })
   );
 });
